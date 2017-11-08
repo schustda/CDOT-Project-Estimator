e436fb91eb00a862551c5cf97068077aa42cf702
schustda <douglas.schuster303@gmail.com>
2017-11-08 16:48:13 -0700

    updates to app

--- app/web_app.py
+++ app/web_app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from sklearn.externals import joblib
-from src.model import CDOTModel
+from src.model.model import CDOTModel
 import locale
 
-
 app = Flask(__name__)
-
-
 locale.setlocale(locale.LC_ALL,'')
 
 @app.route('/', methods=['GET'])
@@ -23,10 +20,10 @@ def predict():
     feature_url = 'https://docs.google.com/spreadsheets/d/1Zt-KDYQP80NmwiWyX0hzukuyBO5YExwohWA9tBqGPhw/export?format=csv&id=1Zt-KDYQP80NmwiWyX0hzukuyBO5YExwohWA9tBqGPhw'
     X_predict = pd.read_csv(feature_url,usecols = ['Quantity']).values.reshape(1,-1)
     data = str(request.form['Item_1'])
-    model = CDOTModel(data)
+    model = CDOTModel()
     pred = locale.currency(model.predict(X_predict),grouping = True)
-    # return render_template('form/predict.html', article=data, predicted=pred)
-    return render_template('predict.html', article=data, predicted = pred)
+    return render_template('predict.html', predicted = pred)
+    # return render_template('predict.html', article=data, predicted = pred)
 
 
 if __name__ == '__main__':
